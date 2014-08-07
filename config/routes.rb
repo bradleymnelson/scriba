c2a6e1a80414ac14ed063415d87e334703c2fc50
Bradley M Nelson <bradley.m.nelson@gmail.com>
2014-08-07 11:51:01 -0400

    good

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/about'
+get 'about', to: 'pages#about'
   resources :transcriptions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'transcriptions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +57,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  def credits
+end
 end
